c7ded56b144c343bbd7ddf26b443915a8618028e
LuanaSCorreia <luanacorreia797@gmail.com>
2022-07-01 12:06:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+let movie = [
+
+ comedy = [
+    {
+    title:'White chicks',
+    director:'Keenen Ivory Wayans',
+    year:2004
+    },
+    {
+    title:'Scary Movie',
+    director:'Keenen Ivory Wayans',
+    year:2000
+    },{
+    title:'Little man',
+    director:'Keenen Ivory Wayans',
+    year:2006
+    }
+],
+
+ action = [
+    {
+    title:'Deadpool',
+    director:'Tim Miller',
+    year:'2016'
+    },
+    {
+    title:'Mulan',
+    director:'Niki Caro',
+    year:'2020'
+    },
+    {
+    title:'Doctor strange in the multiverse of madness',
+    director:'Sam Raimi',
+    year:'2022'
+    }
+],
+
+fantasy = [
+    {
+    title:'Alladin',
+    director:'Guy Ritchie',
+    year:'2019'
+    },{
+    title:'Luca',
+    director:'Enrico Casarosa',
+    year:'2021'
+    },{
+    title:'Descendants ',
+    director:'Kenny Ortega',
+    year:'2015'
+    }
+] 
+]
+
+console.log(movie)
+
+let favorite = "White Chicks"
+if(favorite == "Luca"|| favorite == 'White Chicks'){
+ console.log(`My favorit movie is ${favorite}`)
+ } else{ 
+ console.log('NOT EVEN 5 STARS..." ')
+ }
+
+ 
